7a15680621160cc0d5d3fce21450a16586cd8c6b
Trevor Freeman <trevor.freeman@utexas.edu>
2022-02-06 01:48:50 -0600

    Add tests for correct SE and PE output

--- src/python/test/test_parse_design.py
+++ src/python/test/test_parse_design.py
@@ -25,3 +25,25 @@ def test_usage():
         rv, out = getstatusoutput(f'{prg} {flag}')
         assert rv == 0
         assert re.match("usage", out, re.IGNORECASE)
+
+
+# --------------------------------------------------
+def test_single_end():
+    """single end"""
+
+    rv, out = getstatusoutput(f'{prg} test_SE_design.csv')
+    assert rv == 0
+    expected = ('wt_L1_rep1,data/library_123.fastq.gz\n'
+                'wt_L1_rep2,data/library_456.fastq.gz')
+    assert out.strip() == expected
+
+
+# --------------------------------------------------
+def test_paired_end():
+    """paired end"""
+
+    rv, out = getstatusoutput(f'{prg} test_PE_design.csv')
+    assert rv == 0
+    expected = ('wt_L1_rep1,data/library_123_R1.fastq.gz,data/library_123_R2.fastq.gz\n'
+                'wt_L1_rep2,data/library_456_R1.fastq.gz,data/library_456_R2.fastq.gz')
+    assert out.strip() == expected
