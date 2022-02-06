b0525deeba37f4d32bfff2a67576b77642c6b7f0
Trevor Freeman <trevor.freeman@utexas.edu>
2022-02-06 00:57:39 -0600

    Add tests for existence and usage of design parser

--- src/python/test/test_parse_design.py
+++ src/python/test/test_parse_design.py
@@ -2,3 +2,26 @@
 """
 tests for parse_design.py
 """
+
+
+import os
+import re
+from subprocess import getstatusoutput
+
+prg = '../parse_design.py'
+
+# --------------------------------------------------
+def test_exists():
+    """exists"""
+
+    assert os.path.isfile(prg)
+
+
+# --------------------------------------------------
+def test_usage():
+    """usage"""
+
+    for flag in ['-h', '--help']:
+        rv, out = getstatusoutput(f'{prg} {flag}')
+        assert rv == 0
+        assert re.match("usage", out, re.IGNORECASE)
