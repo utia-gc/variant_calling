2446963a536048863c64f1ca3e5d404f030c8a7c
Trevor Freeman <trevor.freeman@utexas.edu>
2022-02-13 02:33:37 -0600

    Add test for no mixed read types

--- src/python/test/test_parse_design.py
+++ src/python/test/test_parse_design.py
@@ -29,11 +29,21 @@ def test_usage():
     
 # --------------------------------------------------
 def test_missing_header():
-    """"no header"""
+    """no header"""
 
     rv, out = getstatusoutput(f'{prg} PE_design_noHeader.csv')
-    assert rv ==1
+    assert rv == 1
     expected = f"ERROR: Samplesheet -> Missing or invalid header.\n\tLINE: HSL-1,wt_control,1,data/HSL-1_R1.fastq.gz,data/HSL-1_R2.fastq.gz"
+    assert out.strip() == expected
+
+
+# --------------------------------------------------
+def test_check_read_types():
+    """mixed read types"""
+
+    rv, out = getstatusoutput(f'{prg} SE_design_mixTypes.csv')
+    assert rv == 1
+    expected = f"ERROR: Samplesheet -> Mixed read types.\n\tLINE: HSL-3,wt_DMSO,1,data/HSL-3_R1.fastq.gz,data/HSL-3_R2.fastq.gz\n\tHSL-4,wt_DMSO,2,data/HSL-4_R1.fastq.gz,data/HSL-4_R2.fastq.gz"
     assert out.strip() == expected
 
 
