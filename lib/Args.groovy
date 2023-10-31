1acad228e45efc212bd3cd5c95da8ed172ef01cb
Trevor F. Freeman <tfreem10@vols.utk.edu>
2023-10-31 12:54:37 -0400

    change: get args from task.ext namespace

--- lib/Args.groovy
+++ lib/Args.groovy
@@ -1,8 +1,32 @@
+/**
+ * Handle and construct arguments to be passed to process command line scripts.
+ *
+ * We see three main areas where arguments can be passed to the command line:
+ * 1. there should be reasonable default arguments
+ * 2. dynamic arguments should be set depending on the state of the sample, e.g. running certain tools in single- or paired-end mode.
+ * 3. users should be able to specify arguments that take precedence over all others if they see fit.
+ * 
+ * The goal of this class is to provide a convenient way to handle these arguments and construct a consensus set of arguments to pass to a command.
+ */
 class Args {
-    LinkedHashMap<String, String> defaults = new LinkedHashMap<String, String>()
-    LinkedHashMap<String, String> dynamics = new LinkedHashMap<String, String>()
-    LinkedHashMap<String, String> users    = new LinkedHashMap<String, String>()
+    LinkedHashMap<String, String> defaults
+    LinkedHashMap<String, String> dynamics
+    LinkedHashMap<String, String> users
 
+    /**
+     * Construct the Args class using maps from the ext directive namespace for a task.
+     */
+    Args(ext) {
+        this.defaults = ext.argsDefault ?: new LinkedHashMap<String, String>()
+        this.dynamics = ext.argsDynamic ?: new LinkedHashMap<String, String>()
+        this.users    = ext.argsUser    ?: new LinkedHashMap<String, String>()
+    }
+
+    /**
+     * Build a String of command line arguments by first merging the arguments and then joining them together.
+     *
+     * @return String consensus command line arguments.
+     */
     public String buildArgsString() {
         LinkedHashMap consensusArgs = mergeArgs()
 
@@ -18,6 +42,16 @@ class Args {
         return builder.toString()
     }
 
+    /**
+     * Merge command line arguments to produce a consensus set of arguments.
+     *
+     * Arguments in following increasing order of precedence:
+     * 1. Default arguments
+     * 2. Dynamic arguments
+     * 3. User supplied arguments
+     *
+     * @return LinkedHashMap consensus command line arguments.
+     */
     private LinkedHashMap mergeArgs() {
         LinkedHashMap mergedArgs = [:]
         mergedArgs += defaults
@@ -27,40 +61,3 @@ class Args {
         return mergedArgs
     }
 }
-
-// public static String buildArgsString(LinkedHashMap args) {
-//     def builder = new StringBuilder()
-//     args.each { parameter, argument ->
-//         // separate key:value pairs by spaces
-//         if (builder.length() > 0) {
-//             builder.append(' ')
-//         }
-//         argument ? builder.append("${parameter} ${argument}") : builder.append("${parameter}")
-//     }
-
-//     return builder.toString()
-// }
-// public static LinkedHashMap deepMerge(LinkedHashMap... maps) {
-//     def mergedMap = [:]
-//     maps.each { map ->
-//         mergedMap += [ *: map ]
-//     }
-
-//     return mergedMap
-// }
-
-// LinkedHashMap<String, String> argsDefault = [
-//     '--param1': 'arg1',
-//     '--flag1': ''
-// ]
-// LinkedHashMap<String, String> argsDynamic = [
-//     '--paired': ''
-// ]
-// LinkedHashMap<String, String> argsUser = [
-//     '--param1': 'different_arg',
-//     '--flag1': ''
-// ]
-
-// def args = new Args()
-
-// println(args)
